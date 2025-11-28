f89a7d3191aba5cceb3e7c9238212815dfacedf7
Entropy <Hzc299792458@outlook.com>
2025-11-28 21:04:14 +0800

    优化

--- GenshinImpactControl/main.py
+++ GenshinImpactControl/main.py
@@ -12,7 +12,6 @@ from utils.gui_utils import GUIController
 class GenshinImpactMusicPlayer:
     def __init__(self):
         self.controller = GUIController()
-        self.speed = 1000
         self.music_score_text = ""
         self.music_score = []
         self.duration = 0.1
@@ -40,25 +39,55 @@ class GenshinImpactMusicPlayer:
             81: "y",
             83: "u",
         }
+        self.min_time = 0.03
+        self.bpm = 240
+        self.tempo = 60 / self.bpm
 
-    def play_midi(self, file_path):
-        # 检测文件是否存在
+
+    def read_midi(self, file_path):
         if not os.path.exists(file_path):
             logger.error(f"file {file_path} not exists")
+            return None
+        return mido.MidiFile(file_path)
+
+    def mode_recognition(self, midi):
+        if midi is None:
+            return None
+
+        # 获取midi中所有音组成的音阶
+        
+    def play_midi(self, file_path):
+        # 检测文件是否存在
+        mid = self.read_midi(file_path)
+        if mid is None:
             return
-        mid = mido.MidiFile(file_path)
+
         for track in mid.tracks:
-            for msg in track:
-                msg = msg.dict()
+            for i in range(len(track)):
+                msg = track[i].dict()
+                print(msg)
                 if "note" not in msg.keys():
                     continue
-                time.sleep(msg["time"] / self.speed)
+                
+                for j in range(i + 1, len(track)):
+                    next_msg_time = track[j + 1].dict()["time"]
+                    if next_msg_time > 0:
+                        break
+                
+                delete_time = msg["time"] / mid.ticks_per_beat * self.tempo
+                if self.min_time < delete_time:
+                    time.sleep(delete_time - (self.min_time if next_msg_time / mid.ticks_per_beat * self.tempo < self.min_time else 0))
+                elif 0 < msg["time"]:
+                    time.sleep(delete_time + self.min_time)
+
                 if msg["note"] not in self.map.keys():
                     logger.error(f"note {msg['note']} not in map")
                     continue
-                if msg["type"] == "note_on":
+                if msg["type"] == "note_on" and msg["velocity"] > 0:
+                    logger.info(f"press {self.map[msg['note']]}")
                     self.controller.key(self.map[msg["note"]], True)
-                elif msg["type"] == "note_off":
+                elif msg["type"] == "note_off" or msg["velocity"] == 0:
+                    logger.info(f"release {self.map[msg['note']]}")
                     self.controller.key(self.map[msg["note"]], False)
 
 
@@ -81,11 +110,16 @@ class GenshinImpactMusicPlayer:
                 continues = []
             downs = [i for i in downs.strip()]
             self.music_score.append((downs, continues))
+
+    def __del__(self):
+        for key in self.map.values():
+            self.controller.key(key, False)
             
 if __name__ == "__main__":
     # 检测按下某快捷键开始播放
     time.sleep(1)
 
     music_player = GenshinImpactMusicPlayer()
+    # music_player.play_midi("./data/music/2.mid")
     music_player.play_midi("./data/music/离月_music.midi")
     # music_player.play_music_score()
